10547b1539f578deec46ce97c270f38f3b5ff62a
row_gregory <rowgregory@gmail.com>
2018-09-12 23:01:10 -0400

    friend.js change

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,28 +1,28 @@
 const friendsArr = [
   {
-    "name":"Devon",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[5,1,4,4,5,1,2,5,4,1]
+    "name":"",
+    "photo":"",
+    "scores":[]
   },
   {
-    "name":"Greg",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[3,2,2,5,4,4,2,1,5,3]
+    "name":"",
+    "photo":"",
+    "scores":[]
   },
   {
-    "name":"Scott",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[4,3,4,5,1,3,4,3,3,2]
+    "name":"",
+    "photo":"",
+    "scores":[]
   },
   {
-    "name":"Josh",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[3,2,5,5,1,2,3,1,5,2]
+    "name":"",
+    "photo":"",
+    "scores":[]
   },
   {
-    "name":"Jake",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[4,5,3,3,4,5,1,4,3,3]
+    "name":"",
+    "photo":"",
+    "scores":[]
   },
 ];
 
