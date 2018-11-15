602d5cc7389fc9d6d56ed5675fd481340cd9372f
row_gregory <rowgregory@gmail.com>
2018-11-15 18:36:58 -0500

    added gif to readme

--- app/public/logic.js
+++ app/public/logic.js
@@ -60,19 +60,6 @@ $('#sbmtBtn').on('click', (event) => {
 
 
 
-// select a file for future projects
-// document.getElementById('get_file').onclick = function() { 
-//     document.getElementById('my_file').click();
-// };
-
-
-
-// $('input[type=file]').change(function (e) {
-//     $('#customfileupload').html($(this).val());
-// });
-
-
-
 
 
 
